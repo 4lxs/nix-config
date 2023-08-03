5082ace96ef075eef2410ad3781cf04141f64db0
Svl <66408983+4lxs@users.noreply.github.com>
2023-08-03 15:24:21 +0200

    vim changes

--- home/features/nvim/config/lua/plugins/init.lua
+++ home/features/nvim/config/lua/plugins/init.lua
@@ -1,131 +1,108 @@
-local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-local is_bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-	vim.cmd([[packadd packer.nvim]])
-	is_bootstrap = true
+local lazypath = vim.fn.stdpath('data') .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system { 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath }
 end
+vim.opt.rtp:prepend(lazypath)
 
 local api = vim.api
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-	return
-end
 
--- Highlight on yank
-local rpcGrp = api.nvim_create_augroup("ReloadPackerConfig", { clear = true })
-api.nvim_create_autocmd("BufWritePost", {
-	pattern = { "*/plugins/init.lua" },
-	callback = packer.sync,
-	group = rpcGrp,
-})
+require('lazy').setup {
+	-- "nvim-lua/plenary.nvim",
+	-- "folke/tokyonight.nvim",
+	-- "ggandor/leap.nvim",
+	-- "nvim-lualine/lualine.nvim",
+	-- "lukas-reineke/indent-blankline.nvim",
+	-- "lewis6991/impatient.nvim",
+	-- "tpope/vim-sleuth",
+	-- "kyazdani42/nvim-web-devicons",
+	-- "xeluxee/competitest.nvim",
+	-- "windwp/nvim-autopairs",
+	-- "ThePrimeagen/harpoon",
+	-- { "akinsho/bufferline.nvim", tag = "v2.*" },
+	-- "folke/neodev.nvim",
+	-- "numToStr/Comment.nvim",
+	-- "kylechui/nvim-surround",
+	-- "folke/which-key.nvim",
+	-- "danymat/neogen",
 
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
-})
+	-- -- better ui
+	-- "rcarriga/nvim-notify",
+	-- "MunifTanjim/nui.nvim",
 
-local plugins = {
-	"wbthomason/packer.nvim",
-	"nvim-lua/plenary.nvim",
-	"folke/tokyonight.nvim",
-	"ggandor/leap.nvim",
-	"nvim-lualine/lualine.nvim",
-	"lukas-reineke/indent-blankline.nvim",
-	"lewis6991/impatient.nvim",
-	"tpope/vim-sleuth",
-	"kyazdani42/nvim-web-devicons",
-	"xeluxee/competitest.nvim",
-	"windwp/nvim-autopairs",
-	"ThePrimeagen/harpoon",
-	{ "akinsho/bufferline.nvim", tag = "v2.*" },
-	"folke/neodev.nvim",
-	"numToStr/Comment.nvim",
-	"kylechui/nvim-surround",
-	"folke/which-key.nvim",
-	"danymat/neogen",
+	-- -- writing
+	-- { "lervag/vimtex", ft = { "tex", "latex" } },
+	-- {
+	-- 	"iamcco/markdown-preview.nvim",
+	-- 	run = "cd app && npm install",
+	-- 	ft = { "markdown" },
+	-- 	setup = 'require("plugins.markdown-preview-setup")',
+	-- },
 
-	-- better ui
-	"rcarriga/nvim-notify",
-	"MunifTanjim/nui.nvim",
+	-- -- lsp (config for all plugins is in lsp.lua)
+	-- "onsails/lspkind.nvim",
+	-- "williamboman/mason.nvim",
+	-- "williamboman/mason-lspconfig.nvim",
+	-- "jayp0521/mason-null-ls.nvim",
+	-- "jose-elias-alvarez/null-ls.nvim",
+	-- "neovim/nvim-lspconfig",
+	-- "j-hui/fidget.nvim",
+	-- "folke/trouble.nvim",
+	-- "simrat39/rust-tools.nvim",
 
-	-- writing
-	{ "lervag/vimtex", ft = { "tex", "latex" } },
-	{
-		"iamcco/markdown-preview.nvim",
-		run = "cd app && npm install",
-		ft = { "markdown" },
-		setup = 'require("plugins.markdown-preview-setup")',
-	},
+	-- -- debugging
+	-- "mfussenegger/nvim-dap",
+	-- "rcarriga/nvim-dap-ui",
+	-- "jayp0521/mason-nvim-dap.nvim",
+	-- "theHamsta/nvim-dap-virtual-text",
 
-	-- lsp (config for all plugins is in lsp.lua)
-	"onsails/lspkind.nvim",
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"jayp0521/mason-null-ls.nvim",
-	"jose-elias-alvarez/null-ls.nvim",
-	"neovim/nvim-lspconfig",
-	"j-hui/fidget.nvim",
-	"folke/trouble.nvim",
-	"simrat39/rust-tools.nvim",
+	-- -- autocompletion
+	-- "hrsh7th/cmp-nvim-lsp",
+	-- "hrsh7th/nvim-cmp",
+	-- "saadparwaiz1/cmp_luasnip",
+	-- "L3MON4D3/LuaSnip",
+	-- "hrsh7th/cmp-path",
+	-- "hrsh7th/cmp-buffer",
+	-- "ray-x/lsp_signature.nvim",
+	-- "rafamadriz/friendly-snippets",
 
-	-- debugging
-	"mfussenegger/nvim-dap",
-	"rcarriga/nvim-dap-ui",
-	"jayp0521/mason-nvim-dap.nvim",
-	"theHamsta/nvim-dap-virtual-text",
+	-- -- treesitter
+	-- "nvim-treesitter/nvim-treesitter-textobjects",
+	-- "nvim-treesitter/nvim-treesitter-context",
+	-- "andymass/vim-matchup",
+	-- {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	run = function()
+	-- 		require("nvim-treesitter.install").update({ with_sync = true })
+	-- 	end,
+	-- },
+	-- "nvim-treesitter/playground",
+	-- { "yioneko/nvim-yati", tag = "*" },
 
-	-- autocompletion
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/nvim-cmp",
-	"saadparwaiz1/cmp_luasnip",
-	"L3MON4D3/LuaSnip",
-	"hrsh7th/cmp-path",
-	"hrsh7th/cmp-buffer",
-	"ray-x/lsp_signature.nvim",
-	"rafamadriz/friendly-snippets",
+	-- -- telescope
+	-- { "nvim-telescope/telescope.nvim", tag = "0.1.0" },
+	-- "nvim-telescope/telescope-fzy-native.nvim",
 
-	-- treesitter
-	"nvim-treesitter/nvim-treesitter-textobjects",
-	"nvim-treesitter/nvim-treesitter-context",
-	"andymass/vim-matchup",
-	{
-		"nvim-treesitter/nvim-treesitter",
-		run = function()
-			require("nvim-treesitter.install").update({ with_sync = true })
-		end,
-	},
-	"nvim-treesitter/playground",
-	{ "yioneko/nvim-yati", tag = "*" },
+	-- -- tmux nvim integration
+	-- "christoomey/vim-tmux-navigator",
+	-- "vimpostor/vim-tpipeline",
 
-	-- telescope
-	{ "nvim-telescope/telescope.nvim", tag = "0.1.0" },
-	"nvim-telescope/telescope-fzy-native.nvim",
-
-	-- tmux nvim integration
-	"christoomey/vim-tmux-navigator",
-	"vimpostor/vim-tpipeline",
-
-	-- notetaking
-	"renerocksai/telekasten.nvim",
+	-- -- notetaking
+	-- "renerocksai/telekasten.nvim",
 }
 
-return packer.startup(function(use)
-	for _, plug in ipairs(plugins) do
-		local plugin = type(plug) == "string" and { plug } or plug
-		plugin.config = string.format(
-			'pcall(require, "plugins.%s")',
-			string.gsub(string.lower(string.match(plugin[1], ".+/([^\\.]+)")), "nvim%-", "")
-		)
-		use(plugin)
-	end
+-- return packer.startup(function(use)
+-- 	for _, plug in ipairs(plugins) do
+-- 		local plugin = type(plug) == "string" and { plug } or plug
+-- 		plugin.config = string.format(
+-- 			'pcall(require, "plugins.%s")',
+-- 			string.gsub(string.lower(string.match(plugin[1], ".+/([^\\.]+)")), "nvim%-", "")
+-- 		)
+-- 		use(plugin)
+-- 	end
 
-	-- first launch
-	if is_bootstrap then
-		print("Plugins are being installed")
-		packer.sync()
-	end
-end)
+-- 	-- first launch
+-- 	if is_bootstrap then
+-- 		print("Plugins are being installed")
+-- 		packer.sync()
+-- 	end
+-- end)
